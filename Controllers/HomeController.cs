9fcb7895ce56fbffe44ce760689808bfd168a788
Lucas Tjor <tjor75@gmail.com>
2024-06-24 09:40:00 -0300

    Creación del Proyecto .NET Core MVC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PROG_2024_TP05_Tjor.Models;
+
+namespace PROG_2024_TP05_Tjor.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PROG_2024_TP05_Tjor.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
