560d9c36757ada6bdb80051af921ff5b7105ec74
Lucas Tjor <tjor75@gmail.com>
2024-06-24 09:48:08 -0300

    Primeros cambios: empezar Escape.cs

---
+++ Models/Escape.cs
@@ -0,0 +1,10 @@
+static class Escape {
+    private static string[] incognitasSalas = new string[5];
+    private static int estadoJuego = 1;
+
+    private static void InicializarJuego(){}
+    public static int GetEstadoJuego(){}
+    public static bool ResolverSala(int Sala, string Incognita){
+
+    }
+}
